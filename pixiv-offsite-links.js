bc9943e71cfba999783fb4ee3c0d88a0ddec1770
saori m <trhvmn.abcn@gmail.com>
2025-01-19 14:39:49 -0300

    pixiv-offsite-links: fix the script not working, again

--- pixiv-offsite-links.js
+++ pixiv-offsite-links.js
@@ -1,19 +1,15 @@
 // ==UserScript==
 // @name        Pixiv Skip Offsite Link Interceptor
 // @namespace   https://github.com/saoricake/userscripts
-// @version     1.1.1
+// @version     1.1.2
 // @author      saori
 // @description Skips the intermediate page when clicking an offsite link on Pixiv.
-// @match       https://www.pixiv.net/*
+// @match       *://*.pixiv.net/jump.php?*
+// @run-at      document-end
 // @downloadURL https://github.com/saoricake/userscripts/raw/main/pixiv-offsite-links.user.js
 // @updateURL   https://github.com/saoricake/userscripts/raw/main/pixiv-offsite-links.user.js
 // ==/UserScript==
 
-(function() {
-  const links = document.querySelectorAll('a[href^="/jump.php"]');
-  for (const a of links) {
-    const href = decodeURIComponent(a.search).split('http');
-    href.shift();
-    a.href = 'http' + href.join('');
-  }
-})();
+location.replace(
+  decodeURIComponent(location.search).replace(/\?(?:url=)?/, '')
+);
