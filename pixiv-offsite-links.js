e4da2bb67e6c6fb6d3665d1c8eae609d81935fec
saori m <trhvmn.abcn@gmail.com>
2026-04-25 18:24:45 -0300

    pixiv-offsite-links: change run-at to document-start

--- pixiv-offsite-links.js
+++ pixiv-offsite-links.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name        Pixiv Skip Offsite Link Interceptor
 // @namespace   https://github.com/saoricake/userscripts
-// @version     1.1.2
+// @version     1.2
 // @author      saori
 // @description Skips the intermediate page when clicking an offsite link on Pixiv.
-// @match       *://*.pixiv.net/jump.php?*
-// @run-at      document-end
+// @match       *://www.pixiv.net/jump.php?*
+// @run-at      document-start
 // @downloadURL https://github.com/saoricake/userscripts/raw/main/pixiv-offsite-links.user.js
 // @updateURL   https://github.com/saoricake/userscripts/raw/main/pixiv-offsite-links.user.js
 // ==/UserScript==
 
 location.replace(
-  decodeURIComponent(location.search).replace(/\?(?:url=)?/, '')
+  decodeURIComponent(location.search).replace(/^\?(?:url=)?/, "")
 );
